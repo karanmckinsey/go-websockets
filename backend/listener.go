4665dbbc3d568218db4deb1a049a4305ad89c6b8
karanmckinsey <93206648+karanmckinsey@users.noreply.github.com>
2022-03-31 09:50:39 +0530

    add ws listener

---
+++ backend/listener.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+func listener(conn *websocket.Conn) {
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return 
+		}
+		fmt.Println("Message Received", string(p))
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	// We'll need to check the origin of our connection this will allow us to make requests from our React
+  	// development server to here. For now, we'll do no checking and just allow any connection
+  	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func serveWS(w http.ResponseWriter, r *http.Request) {
+	// upgrade the http connection to websocket connection 
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// continously listen for new incoming messages 
+	listener(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
